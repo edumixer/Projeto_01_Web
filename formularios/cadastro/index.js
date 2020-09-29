4ee785f9dcdb1b06002b5aafc4d8ccf63e077a76
edumixer <edu.cena.wf@gmail.com>
2020-09-29 17:18:20 -0300

    Adicionando a pasta formularios com as subpastas login e cadastro

---
+++ formularios/cadastro/index.js
@@ -0,0 +1,110 @@
+var dados = [];
+var banco = window.localStorage;
+
+$(document).ready(function() {
+    fLocalEventosCLick();
+    var db = banco.getItem("dadosUsuarios")
+    if( db !== null) {
+        dados = JSON.parse(db);   
+    } else {
+        dados = [];
+    }
+function fLocalEventosCLick() {
+    $("#salvar-formulario").click(function() {
+
+        fLocalValidaCampos("nome");
+        fLocalValidaCampos("sobrenome");
+        fLocalValidaCampos("email");
+        fLocalValidaCampos("senha");
+        fLocalValidaCampos("confirma_senha");
+
+        fLocalValidaSenha();
+        
+        var aux = {};
+        aux.nome = $("#nome").val();
+        aux.sobrenome = $("#sobrenome").val();
+        aux.email = $("#email").val();
+        aux.senha =  $("#senha").val();
+        aux.confirma_senha =  $("#confirma_senha").val();
+        
+        dados.push(aux);
+        
+        banco.setItem("dadosUsuarios", JSON.stringify(dados));
+        
+        if (fLocalValidaTodos()) {
+            fLocalLimpaCampos();
+            window.location.href = "../home/index.html"
+        } 
+            
+    });
+}
+    
+    function fLocalValidaCampos(idCampo) {
+        var valor = $("#" + idCampo).val();
+
+        if(valor == "") {
+            $("#" + idCampo).addClass("campo-errado");
+            return false;
+        } else {
+            $("#" + idCampo).removeClass("campo-errado");
+            return true;
+        }
+    }
+
+    function fLocalValidaTodos() {
+        
+        
+        if(fLocalValidaCampos("nome") === false) {
+            return false;
+        } 
+        if (fLocalValidaCampos("sobrenome") === false) {
+            return false;
+        }
+        if (fLocalValidaCampos("email") === false) {
+            return false;
+        }
+        if (fLocalValidaCampos("senha") === false) {
+            return false;
+        }
+        if (fLocalValidaCampos("confirma_senha") === false) {
+            return false;
+        }
+        if (fLocalValidaSenha() === false) {
+            return false;
+        }
+        else {
+            return true;
+        }
+        
+    }   
+
+    function fLocalValidaSenha() {
+        var senha = $("#senha").val();
+        var confirma_senha = $("#confirma_senha").val();
+        
+        if(senha != "") {
+            if(senha != confirma_senha) {
+                $("#confirma_senha").addClass("campo-errado");
+                return false;
+            }
+            else {
+                $("#confirma_senha").removeClass("campo-errado");
+                return true;
+            }
+        }
+    }
+
+    function fLocalLimpaCampos() {
+        $("#nome").val("");
+        $("#sobrenome").val("");
+        $("#email").val("");
+        $("#senha").val("");
+        $("#confirma_senha").val("");
+    }
+    
+
+    
+});
+
+
+
